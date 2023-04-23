cb5f333e874df855b18c24420b3f142e0396b42c
jamesm <jamesmyddelton@gmail.com>
2023-04-23 19:31:33 +0100

    Updated

--- main.py
+++ main.py
@@ -11,7 +11,14 @@ print(a.keys())
 a = {"1":1, "2":2, "3":3}
 print(a.items())
 
+# Find Same Keys
 a = {"1":1, "2":2, "3":3}
 b = {"2":2, "3":3, "4":4}
 print([_ for _ in a.keys() if _ in b.keys()])
+# better way
+c = set(a).intersection(set(b))
+print(list(c))
+# or
+print([_ for _ in a if _ in b])
+
 
